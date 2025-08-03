9af35f1d507d5cc70e165c635cc2bad4cf071f97
ComeONDuDE18 <vibilous6@gmail.com>
2025-08-04 00:29:10 +0200

    maybeLastTime

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,26 +1,19 @@
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
 import { fetchNoteById } from "@/lib/api";
 import NoteDetailsClient from "./NoteDetails.client";
-import { QueryClient } from "@tanstack/react-query";
-
 
 interface PageProps {
   params: { id: string };
-};
-
-
+}
 
 export default async function NoteDetails({ params }: PageProps) {
-  
-     const queryClient = new QueryClient();
-    const {id} = await params;
-  
-      
-    await queryClient.prefetchQuery({
+  const queryClient = new QueryClient();
+  const { id } = params; 
+
+  await queryClient.prefetchQuery({
     queryKey: ['note', id],
     queryFn: () => fetchNoteById(id),
   });
-
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
@@ -28,3 +21,4 @@ export default async function NoteDetails({ params }: PageProps) {
     </HydrationBoundary>
   );
 }
+
